fb2f7c357ff09f58ec02225405b1b52b49f8281a
Shanmuk Chowdary <shhannu02@gmail.com>
2025-06-13 11:47:07 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dashboardapp';
+  students: { name: string; grade: string }[] = [];
+  name: string = '';
+  grade: string = '';
+  addProfile() {
+    if (this.name && this.grade) {
+      this.students.push({ name: this.name, grade: this.grade });
+      this.name = '';
+      this.grade = '';
+    }
+
 }
+}
+
